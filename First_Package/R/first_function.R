69344bcd1287e9a1a97b43669d4cbdf3b988cbea
TahaBayaz <41266155+TahaBayaz@users.noreply.github.com>
2020-08-20 11:28:57 +0300

    Creating the first package

---
+++ First_Package/R/first_function.R
@@ -0,0 +1,8 @@
+#'@title Title of The First Function
+#'@description First function gets a number and returns the square root of that number.
+#'@param x Numeric input parameter. Default = 1.
+#'@return The function returns the square root of the input.
+#'@export
+square_root <- function(x=1){
+  return(x^0.5)
+}
